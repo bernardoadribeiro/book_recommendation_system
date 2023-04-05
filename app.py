f8716202f29d433236904b0807be6bb29f36595e
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2023-04-05 06:02:48 -0300

    stable v4

--- app.py
+++ app.py
@@ -2,61 +2,27 @@ from flask import Flask, abort, jsonify, render_template, request
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
-import sqlalchemy
-from data import load_data
 from helper import get_recommended_books
 
-# Import routes (to register blueprints)
-# from routes import books
 
 
 app = Flask(__name__)
 app.app_context().push()
 app.secret_key = 'something_secret'
 
-""" Database Setup
-"""
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
-
-db = SQLAlchemy()
-db.init_app(app)
-migrate = Migrate(app, db, directory='./migrations')
-
-from sqlalchemy_serializer import SerializerMixin
-
-class Books(db.Model, SerializerMixin):
-    ISBN = db.Column(db.Integer, primary_key=True)
-    Book_Title = db.Column(db.String)
-    Book_Author = db.Column(db.String)
-    Year_Of_Publication = db.Column(db.Integer)
-    Publisher = db.Column(db.String)
-    Image_URL = db.Column(db.String)
-    Clusters = db.Column(db.Integer)
-
-
-# engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI']) 
-# df_book = pd.read_csv('data/books.csv')
-# df_book.to_sql('books', engine, if_exists='append')
-# df_clus = pd.read_csv('data/ratings_with_clusters.csv')
-# df_clus.to_sql('books', engine, if_exists='append')
 
 """ Routes
 """
 @app.route('/', methods=['GET'])
 def hello():
     df_users = pd.read_csv('data/ratings_with_clusters.csv')
-
     user_ids = df_users['user_id'].unique().tolist()
     return render_template('index.html', users=user_ids)
 
-@app.route('/books', methods=['GET'])
-def books():
-    return render_template('books/index.html')
 
 @app.route('/users/', methods=['GET'])
 def users():
     df_users = pd.read_csv('data/ratings_with_clusters.csv')
-
     user_ids = df_users['user_id'].unique().tolist()
 
     return jsonify(user_ids)
@@ -111,10 +77,3 @@ if __name__ == '__main__':
         use_reloader=True,
         debug=True,
     )
-
-# Register routes
-# app.register_blueprint(books.books_bp, url_prefix='/')
-
-
-# Import database models from models
-# from models import Books
