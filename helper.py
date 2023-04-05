a965265d3eb3988748caa1ebeaef0ece29922cda
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2023-04-05 06:09:02 -0300

    chore adjusts

--- helper.py
+++ helper.py
@@ -2,20 +2,20 @@ import pandas as pd
 
 
 def get_recommended_books(df:pd.DataFrame, user_id:int, n_books:int = None) -> list:
-    """ Retorna `n_books`Recomendação de livros para cada usuário.
-        Returns a list of `ISBNs` of recommended books for a user.
+    """
+        Returns `n_books` books recommended to the user
     """
      
-     # Encontra o cluster do usuário
+     # find the user's Cluster
     user_cluster = df.loc[df['user_id'] == user_id, 'Cluster'].iloc[0]
 
-    # Filtra os usuários do mesmo cluster e seleciona os livros bem avaliados por eles
+    # Filter the books by the user's cluster, and select the top `n_books` books
     similar_users = df.loc[df['Cluster'] == user_cluster]
     livros_melhor_avaliados = similar_users.loc[similar_users['Book_Rating'] >= 4.0, 'ISBN'].unique()
 
-    # Se o número de livros recomendados não for especificado, retorna todos os livros bem avaliados
+    # If n_books isn't inputed, Returns all books that is recommended to the user
     if n_books is None:
         return livros_melhor_avaliados
-    # Caso contrário, retorna os primeiros n livros da lista
+    # In other case, return the n_books most voted of the user's cluster
     else:
         return livros_melhor_avaliados[:n_books]
